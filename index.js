4a5c713decbab5e2baece07cf0298d06e492be98
userdan <root.danx20@gmail.com>
2020-05-18 17:58:59 +0800

    Creating a simple web server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
+const http = require('http');
 
+// - -  -  -  -  - - -  -  -  -  - 
+// -- FILES
+
+/*
 // Blocking, synchronous way
-// const inputIn = fs.readFileSync('./Came-from-course/txt/input.txt', 'utf8');
-// console.log(inputIn);
-// const InputOut = `New avocado is availabe on: ${inputIn}. \n Created on ${Date.now()}`;
-// fs.writeFileSync(`./Came-from-course/txt/output.txt`, InputOut);
-// console.log(InputOut);
+const inputIn = fs.readFileSync('./Came-from-course/txt/input.txt', 'utf8');
+console.log(inputIn);
+const InputOut = `New avocado is availabe on: ${inputIn}. \n Created on ${Date.now()}`;
+fs.writeFileSync(`./Came-from-course/txt/output.txt`, InputOut);
+console.log(InputOut);
 
 // Non-blocking, Asynchronous way
 fs.readFile('./Came-from-course/txt/start.txt', 'utf8',(err, data1) => {
@@ -13,12 +18,24 @@ fs.readFile('./Came-from-course/txt/start.txt', 'utf8',(err, data1) => {
         console.log(data2);
         fs.readFile(`./Came-from-course/txt/append.txt`, 'utf8',(err, data3) => {
             console.log(data3);
-
-        //location,2function,utf,error=log
-        fs.writeFile('./Came-from-course/txt/final.txt',`${data2}\n${data3}`, 'utf8', err => {
+            fs.writeFile('./Came-from-course/txt/final.txt',`${data2}\n${data3}`, 'utf8', err => {
             console.log('Your file has been written');
-        }  )    
+        })    
         });
     });
 });
 console.log('File is written!');
+
+*/
+
+// - -  -  -  -  - - -  -  -  -  - 
+// -- SERVER
+
+const server = http.createServer((req, res) => { 
+    // console.log(req);
+    res.end('Hello from the server'); // to execute on the browser
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000')
+})
