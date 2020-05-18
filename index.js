cfda7f3f290926e89ea3974be668dd622c8894e7
userdan <root.danx20@gmail.com>
2020-05-18 15:57:43 +0800

    Reading and Writing Files Asynchronously

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const fs = require('fs'); // access or reading data
+const fs = require('fs');
 
-const textIn = fs.readFileSync('./Came-from-course/txt/input.txt', 'utf8');
-console.log(textIn);
+// Blocking, synchronous way
+// const inputIn = fs.readFileSync('./Came-from-course/txt/input.txt', 'utf8');
+// console.log(inputIn);
+// const InputOut = `New avocado is availabe on: ${inputIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync(`./Came-from-course/txt/output.txt`, InputOut);
+// console.log(InputOut);
 
-// text
-// fs.writeFile
-//console.log(fs.writeFile)
+// Non-blocking, Asynchronous way
+fs.readFile('./Came-from-course/txt/start.txt', 'utf8',(err, data1) => {
+    fs.readFile(`./Came-from-course/txt/${data1}.txt`, 'utf8',(err, data2) => {
+        console.log(data2);
+        fs.readFile(`./Came-from-course/txt/append.txt`, 'utf8',(err, data3) => {
+            console.log(data3);
 
-
-const textOut = `This is the avocado : ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./Came-from-course/txt/output.txt', textOut);
-
-console.log('Text written!');
+        //location,2function,utf,error=log
+        fs.writeFile('./Came-from-course/txt/final.txt',`${data2}\n${data3}`, 'utf8', err => {
+            console.log('Your file has been written');
+        }  )    
+        });
+    });
+});
+console.log('File is written!');
